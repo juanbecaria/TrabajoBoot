9532ba4a33a3176873a07eb34dea0b11f7eb8ee0
juanbecaria <juanbecaria@gmail.com>
2016-10-18 17:38:10 -0300

    pasando los objetos de json a los de dominio

--- src/main/java/web/Proxy.java
+++ src/main/java/web/Proxy.java
@@ -1,22 +1,35 @@
 package web;
 
-import negocio.Atmosfera;
-import negocio.DiaActual;
-import negocio.Pronostico;
+import negocio.*;
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import soporte.*;
 
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by juanb on 10/18/2016.
  */
 @Component
 public class Proxy {
-    private AtmosferaBuilder atmosferaBuilder;
-    private DiaActualBuilder diaActualBuilder;
+    @Autowired
+    private static AtmosferaBuilder atmosferaBuilder;
+    @Autowired
+    private static DiaActualBuilder diaActualBuilder;
+    @Autowired
+    private static LocalidadBuilder localidadBuilder;
+    @Autowired
+    private static PronosticoBuilder pronosticoBuilder;
+    @Autowired
+    private static PronosticoExtendidoBuilder pronosticoExtendidoBuilder;
+    @Autowired
+    private  static VientoBuilder vientoBuilder;
+
 
 
 
@@ -34,7 +47,55 @@ public class Proxy {
             e.printStackTrace();
         }
         if(json!=null) {
+
             JSONObject channel = json.getJSONObject("query").getJSONObject("results").getJSONObject("channel");
+
+
+
+            Atmosfera at = atmosferaBuilder.withHumedad((float)channel.getJSONObject("atmosphere").getDouble("humidity"))
+                    .withPresion((float)channel.getJSONObject("atmosphere").getDouble("pressure"))
+                    .withAmbienteAscendente((float)channel.getJSONObject("atmosphere").getDouble("rising"))
+                    .withVisibilidad((float)channel.getJSONObject("atmosphere").getDouble("visibility")).createAtmosfera();
+
+            Localidad l = localidadBuilder.withCiudad(channel.getJSONObject("location").getString("city"))
+                    .withPais(channel.getJSONObject("location").getString("country"))
+                    .withRegion(channel.getJSONObject("location").getString("region")).createLocalidad();
+
+            Viento v = vientoBuilder.withVelocidad((float)channel.getJSONObject("wind").getDouble("speed"))
+                    .withDireccion((float)channel.getJSONObject("wind").getDouble("direction")).createViento();
+
+
+
+            JSONArray jArray=channel.getJSONObject("item").getJSONArray("forecast");
+            List<PronosticoExtendido> list=new ArrayList<>();
+
+            DiaActual da = diaActualBuilder.withTemp((float)channel.getJSONObject("item").getJSONObject("condition").getDouble("temp"))
+                    .withDescripcion(channel.getJSONObject("item").getJSONObject("condition").getString("text"))
+                    .withFecha(jArray.getJSONObject(0).getString("date")).createDiaActual();
+
+
+            for(int i = 1; i<jArray.length();i++)
+            {
+                JSONObject jo = jArray.getJSONObject(i);
+                PronosticoExtendido p = pronosticoExtendidoBuilder.withDescripcion(jo.getString("text"))
+                        .withDia(jo.getString("day"))
+                        .withFecha(jo.getString("date"))
+                        .withTempMax((float)jo.getDouble("high"))
+                        .withTempMin((float)jo.getDouble("low")).createPronosticoExtendido();
+                list.add(p);
+            }
+
+            Pronostico p = pronosticoBuilder.withAtmosfera(at)
+                    .withDiaActual(da)
+                    .withViento(v)
+                    .withLocalidad(l)
+                    .withPronosticoExtendido(list).createPronostico();
+
+            
+
+
+
+
 
 
         }
@@ -52,3 +113,4 @@ public class Proxy {
 
 
 }
+
